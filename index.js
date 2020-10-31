6e8deff1596f14910b97bd0ee4d0ebbbfb2573ff
devenock <enockomondi305@gmail.com>
2020-10-31 16:11:52 -0400

    working on the DB and styles

--- index.js
+++ index.js
@@ -8,47 +8,52 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'login',
+    database: 'Login',
 });
- const app = express();
- app.use(session({
+const app = express();
+app.use(session({
     secret: 'secret',
-	resave: true,
-	saveUninitialized: true
- }));
- app.use(bodyParser.urlencoded({extended:true}));
- app.use(bodyParser.json());
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
- app.get('/', function(request, response){
-     response.sendFile(path.join(__dirname + '/login.html'));
- });
+// styles
+app.use(express.static(__dirname + '/public'));
 
- app.post('/auth', function(request, response){
-     const username = request.body.username;
-     const password = request.body.password;
-     if(username&&password){
-         connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields){
-             if(results.length > 0){
-                 request.session.loggedin = true;
-                 request.session.username = username;
-                 response.redirect('/home');
-             }else{
-                 response.send('Incorrect Username or/and Password');
-             }
-             response.end();
-         });
-     }else{
-        response.send('Please enter Username and Password!');
-		response.end();
-     }
- });
- app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.listen(3000);
+app.post('/auth', function(request, response) {
+    const username = request.body.username;
+    const password = request.body.password;
+    if (username && password) {
+        connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username or/and Password');
+            }
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
+app.get('/home', function(request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
+
+app.listen(3000, () => {
+    console.log('The Server is running successfully');
+});
